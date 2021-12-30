90c152ad87d4bde726f7e9b4d98a49d743abbf21
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-12-30 19:47:28 +0000

    Completed claim/view

--- client/Components/eggDetails.jsx
+++ client/Components/eggDetails.jsx
@@ -4,6 +4,7 @@ import Button from "../UI/button";
 
 const EggDetails = (props) => {
   const [open] = useState(props.targetEgg);
+  const [isClaimed, setIsClaimed] = useState(false);
 
   function onDismiss() {
     props.toggleModal(null);
@@ -13,6 +14,8 @@ const EggDetails = (props) => {
   date = date.split("T");
   date = date[0].split("-");
   const orderedDate = `${date[1]}-${date[2]}-${date[0]}`;
+
+  const claimEggHandler = () => setIsClaimed(true);
 
   return (
     <>
@@ -28,27 +31,48 @@ const EggDetails = (props) => {
         expandOnContentDrag={true}
       >
         <div className={"row justify-align-center flex-column"}>
-          <div className="p1 center-text">
-            <h1>{props.targetEgg.username}&apos;s Egg</h1>
-            <p className="light">
-              <i>Created on {orderedDate}</i>
-            </p>
-          </div>
-
-          <div className="center-text">
-            <h2>Distance from Egg: </h2>
-            <h2 className="password-check">
-              <b>
-                {parseInt(props.targetEgg.howFar)} {props.targetEgg.metric}
-              </b>
-            </h2>
-          </div>
-          <div className="blur" />
+          {!isClaimed && (
+            <div className="p1 center-text">
+              <h1>{props.targetEgg.username}&apos;s Egg</h1>
+              <p className="light">
+                <i>Created on {orderedDate}</i>
+              </p>
+            </div>
+          )}
+          {isClaimed && (
+            <>
+              <div className="p1 center-text egg-details-message">
+                <p>&quot;{props.targetEgg.message}&quot;</p>
+              </div>
+              <img
+                src={props.targetEgg.photoUrl}
+                alt={`${props.username} secret egg`}
+                className="egg-details-img"
+              ></img>
+            </>
+          )}
+          {!isClaimed && (
+            <div className="center-text">
+              <h2>Distance from Egg: </h2>
+              <h2 className="password-check">
+                <b>
+                  {parseInt(props.targetEgg.howFar)} {props.targetEgg.metric}
+                </b>
+              </h2>
+            </div>
+          )}
         </div>
         <Button
-          className="submit-sign-up mb1"
-          text={props.targetEgg.claimable ? "CLAIM EGG!" : "Get Closer"}
-          disabled={!props.targetEgg.claimable}
+          className={`submit-sign-up mb1 ${isClaimed && "claimed-egg-button"}`}
+          text={
+            isClaimed
+              ? "Claimed!"
+              : props.targetEgg.claimable
+              ? "CLAIM EGG!"
+              : "Get Closer"
+          }
+          disabled={!props.targetEgg.claimable || isClaimed}
+          click={claimEggHandler}
         />
       </BottomSheet>
     </>
